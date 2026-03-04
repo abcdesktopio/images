6b566b70464312b39c567363d82329f7554c3f79
Matt307082 <matteobeghelli2003@gmail.com>
2026-03-04 10:27:38 +0100

    build app list for each version

---
+++ buildAppList.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir appLists
+
+for path in ./artifact/*; do
+    if [ -d "$path" ]; then
+        dirname=$(basename "$path")
+        jq -s '.' $path/*.json > appLists/appList.$dirname.json
+    fi
+done
