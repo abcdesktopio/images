56ea389f4eb325ca551222825dfb4bcc11a405f9
Matt307082 <matteobeghelli2003@gmail.com>
2026-03-09 10:34:28 +0100

    add array flattening to jq command

--- buildAppList.sh
+++ buildAppList.sh
@@ -5,6 +5,6 @@ mkdir appLists
 for path in ./artifact/*; do
     if [ -d "$path" ]; then
         dirname=$(basename "$path")
-        jq -s '.[]' $path/*.json > appLists/appList.$dirname.json
+        jq -s 'flatten(1)' $path/*.json > appLists/appList.$dirname.json
     fi
 done
